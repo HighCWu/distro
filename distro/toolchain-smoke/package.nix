17d761fc59516622b80ffa2872599404c80f42de
HighCWu <HighCWu@163.com>
2026-09-25 15:31:31 +0800

    test: validate native wasm exceptions

--- distro/toolchain-smoke/package.nix
+++ distro/toolchain-smoke/package.nix
@@ -53,6 +53,6 @@ pkgs.runCommand "toolchain-smoke-${platform.wasmArch}"
     mkdir -p $out
     clang ${compileFlags} ${cSource} -o $out/smoke-c.wasm
     clang++ ${compileFlags} ${cxxSource} -o $out/smoke-cxx.wasm
-    wasm-validate --enable-memory64 --enable-threads $out/smoke-c.wasm
-    wasm-validate --enable-memory64 --enable-threads $out/smoke-cxx.wasm
+    wasm-validate --enable-memory64 --enable-threads --enable-exceptions $out/smoke-c.wasm
+    wasm-validate --enable-memory64 --enable-threads --enable-exceptions $out/smoke-cxx.wasm
   ''
